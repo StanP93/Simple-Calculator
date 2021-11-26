864a44e3ab8d64f2c46c4aa47819961d07f85998
Stanislau Pranovich <stanislaupranovich@MacBook-Air-Stanislau.local>
2021-11-26 11:43:47 +0300

    add style

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const calc = document.querySelector('.calc');
-const result = document.getElementById('display')
-
+const result = document.getElementById('display');
+const p = document.querySelector('.p-display');
 
 calc.addEventListener('click', function(event){
     const value = event.target.innerText;
 
-    result.innerText += value;
+   
 })
 
